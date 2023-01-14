69567dbb1e7026dce5909e926a0bc9a860828317
Competent50 <122703096+Competent50@users.noreply.github.com>
2023-01-14 22:46:53 +0000

    Add files via upload

---
+++ COLLINS/main.py
@@ -0,0 +1,28 @@
+alien_0 = {'colour' : 'green', 'points' : '5'}
+alien_1 = {'colour' : 'yellow', 'points' : '10'}
+alien_2 = {'colour' : 'red', 'points' : '15'}
+aliens = [alien_0, alien_1, alien_2 ]
+for alien in aliens:
+    print(alien)
+
+
+
+
+    # Use a dictionary to store information about a person you know.
+    # Store their first name, last name, age, and the city in which they live. You
+    # should have keys such as first_name, last_name, age, and city. Print each
+    # piece of information stored in your dictionary
+
+    Tyson = {
+        'first_name': 'Eric',
+        'last_name': 'Worlakatey',
+        'age': '22',
+        'city': 'Bogoso',
+    }
+    print("first_name is: " + Tyson['first_name'].title() + ".")
+    print("last_name is: " + Tyson['last_name'].title() + ".")
+    print("age is: " + Tyson['age'].title() + ".")
+    print("city is: " + Tyson['city'].title() + ".")
+
+
+
